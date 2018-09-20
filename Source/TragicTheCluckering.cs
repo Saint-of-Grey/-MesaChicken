39cac5fcdcd6e7c1cd02dee933890e433bb5ac0b
Alice Cecil <alycecil@gmail.com>
2018-09-19 23:55:19 -0400

    Chicken Gun

---
+++ Source/TragicTheCluckering.cs
@@ -0,0 +1,17 @@
+﻿using Verse;
+
+namespace TragicTheCluckering
+{
+    public class TragicTheCluckering : Mod
+    {
+        public TragicTheCluckering(ModContentPack content) : base(content)
+        {
+        }
+    }
+
+    static class Constants
+    {
+        public static ThingDef TragicChicken = DefDatabase<ThingDef>.GetNamed("ChickenTragic");
+        public static PawnKindDef TragicChickenKind = DefDatabase<PawnKindDef>.GetNamed("ChickenTragic");
+    }
+}
---
+++ Source/TransFormHediffWithComps.cs
@@ -0,0 +1,55 @@
+using TragicTheCluckering;
+using Verse;
+
+namespace TragicTheCluckering
+{
+    public class TransFormHediffWithComps : HediffWithComps
+    {
+        private ThingDef trueForm;
+        private bool didIt;
+
+        public virtual ThingDef transformInto => Constants.TragicChicken;
+        public virtual PawnKindDef transformIntoKind => Constants.TragicChickenKind;
+
+
+//        public override void PostRemoved()
+//        {
+//            TransformPawn(trueForm);
+//            base.PostRemoved();
+//        }
+
+        private int tick = 0;
+        public override void Tick()
+        {
+            tick++;
+            if(didIt || tick < 7) return;
+            
+            trueForm = pawn.def;
+            TransformPawn(transformInto, transformIntoKind);
+
+            didIt = true;
+        }
+
+        private void TransformPawn(ThingDef @into, PawnKindDef intoKind)
+        {
+            Log.Error("Performing Transform on " + pawn + " to a " + into);
+            if (into == null || into == pawn.def) return;
+
+            var map = pawn.Map;
+            RegionListersUpdater.DeregisterInRegions(pawn, map);
+            pawn.def = into;
+
+            pawn.ChangeKind(intoKind);
+            RegionListersUpdater.RegisterInRegions(pawn, map);
+
+            map.mapPawns.UpdateRegistryForPawn(pawn);
+
+            //save the pawn
+            pawn.ExposeData();
+
+            //pawn.SpawnSetup(map, true);
+        }
+
+        public override bool ShouldRemove => didIt;
+    }
+}
